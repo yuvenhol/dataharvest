fa39625726f0a8ece2cb7e832b9ef114ffa330ba
lee0624 <1090994417@qq.com>
2024-07-02 18:51:44 +0800

    commit merge baijiahao into baidu

--- src/dataharvest/purifier/__init__.py
+++ src/dataharvest/purifier/__init__.py
@@ -2,7 +2,7 @@ from dataharvest.purifier.baidu_purifier import BaiduBaikePurifier
 from dataharvest.purifier.common_purifier import CommonPurifier
 from dataharvest.purifier.sohu_purifier import SohuPurifier
 from dataharvest.purifier.sll_purifier import SllPurifier
-from dataharvest.purifier.baijiahao_purifier import BaiJiaHaoPurifier
+from dataharvest.purifier.baidu_purifier import BaiJiaHaoPurifier
 from dataharvest.purifier.wangyi_purifier import WangYiPurifier
 from dataharvest.purifier.sogou_purifier import SogouBaiKePurifier
 from dataharvest.purifier.purifier import AutoPurifier, BasePurifier
