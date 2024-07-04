e31c417e3b4283497e0efb0720739f3b9081106c
lee0624 <1090994417@qq.com>
2024-07-04 16:17:59 +0800

    commit remove zhihu purifier

--- src/dataharvest/purifier/__init__.py
+++ src/dataharvest/purifier/__init__.py
@@ -1,21 +1,20 @@
-from dataharvest.purifier.baidu_purifier import BaiduBaikePurifier
-from dataharvest.purifier.common_purifier import CommonPurifier
-from dataharvest.purifier.sohu_purifier import SohuPurifier
-from dataharvest.purifier.sll_purifier import SllPurifier
 from dataharvest.purifier.baidu_purifier import BaiJiaHaoPurifier
-from dataharvest.purifier.wangyi_purifier import WangYiPurifier
-from dataharvest.purifier.sogou_purifier import SogouBaiKePurifier
-from dataharvest.purifier.zhihu_purifier import ZhihuPurifier
+from dataharvest.purifier.baidu_purifier import BaiduBaikePurifier
 from dataharvest.purifier.bilibili_purifier import BilibiliPurifier
-from dataharvest.purifier.qqnew_purifier import QQNewPurifier
-from dataharvest.purifier.sobaike_purifier import SoBaikePurifier
-from dataharvest.purifier.toutiao_purifier import ToutiaoPurifier
-from dataharvest.purifier.wechat_purifier import WechatPurifier
+from dataharvest.purifier.common_purifier import CommonPurifier
 from dataharvest.purifier.purifier import AutoPurifier, BasePurifier
+from dataharvest.purifier.qqnew_purifier import QQNewPurifier
+from dataharvest.purifier.sll_purifier import SllPurifier
+from dataharvest.purifier.sobaike_purifier import SoBaikePurifier
+from dataharvest.purifier.sogou_purifier import SogouBaiKePurifier
+from dataharvest.purifier.sohu_purifier import SohuPurifier
+from dataharvest.purifier.toutiao_purifier import ToutiaoPurifier
+from dataharvest.purifier.wangyi_purifier import WangYiPurifier
+from dataharvest.purifier.wechat_purifier import WechatPurifier
 
 __all__ = ["BasePurifier", "CommonPurifier", "AutoPurifier", "BaiduBaikePurifier",
            "SohuPurifier", "SllPurifier", "BaiJiaHaoPurifier", "WangYiPurifier",
-           "SogouBaiKePurifier", "ZhihuPurifier", "BilibiliPurifier", "QQNewPurifier",
+           "SogouBaiKePurifier", "BilibiliPurifier", "QQNewPurifier",
            "SoBaikePurifier", "ToutiaoPurifier", "WechatPurifier"]
 
 
